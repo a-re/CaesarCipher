8dfc87c105b936224391cd9fa6091d0af42a7949
Alex Restifo <calix1999@gmail.com>
2014-11-24 17:58:08 -0500

    Big changes! It recognizes and changes capital letters accordingly and also has an OPTIONAL parameter that will force all-lower or all-upper output.

--- Program.cs
+++ Program.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 class Program
 {
-    static readonly char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+    static readonly char[] alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
     static void Main(string[] args)
     {
-        Console.WriteLine(CaesarEncrypt("hello", 2));
+        Console.WriteLine(CaesarEncrypt("Hello", 1));
         Console.ReadKey();
     }
 
-    public static string CaesarEncrypt(string str, int rot)
+    public static string CaesarEncrypt(string str, int rot, bool? lower = null) /* A nullable boolean! */
     {
-        char[] inStr = str.ToLower().ToCharArray();
+        int l, len = alpha.Length;
+        char[] inStr = lower == null ? str.ToCharArray() : (bool)lower ? str.ToLower().ToCharArray() : str.ToUpper().ToCharArray();
         char[] outC = new char[inStr.Length];
         Dictionary<char, int> dict = new Dictionary<char, int>();
-        for (int x = 0; x < alpha.Length; x++) { dict.Add(alpha[x], x); }
+        for (int x = 0; x < len; x++) { dict.Add(alpha[x], x); }
         for (int i = 0; i < inStr.Length; i++)
         {
-            int l;
             dict.TryGetValue(inStr[i], out l);
-            outC[i] = inStr[i].Equals(' ') ? ' ' : alpha[(l + rot) % alpha.Length];
+            outC[i] = inStr[i].Equals(' ') ? ' ' : char.IsUpper(inStr[i]) ? alpha[l + rot % ((len / 2) + len / 2)] : alpha[(l + rot) % (len / 2)]; 
         }
         return new string(outC);
     }
